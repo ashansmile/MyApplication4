502ff2799397678751be5f037ff86b49b8ea6aa1
ashan <1203>
2016-09-30 14:36:36 +0800

    2

---
+++ app/src/main/java/com/js/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.js.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        int  a = 0;
+        int b = 0;
+    }
+}
